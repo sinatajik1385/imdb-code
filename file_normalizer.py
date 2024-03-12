6c15881a364c951d7d25d17bfd436ac0df1c8bc3
sinatajik1385 <162894663+sinatajik1385@users.noreply.github.com>
2024-03-12 15:40:55 +0330

    Update file_normalizer.py

--- file_normalizer.py
+++ file_normalizer.py
@@ -2,9 +2,9 @@ import pandas as pd
 import numpy as np
 class from_tsv_to_sql ():
     def titleEpisode_cleanup() :
-        dictionary_seasonnumber_episodenumber = []
         df = pd.read_csv ("titleEpisode.tsv",chunksize=50000, sep= "\t")
         for chunks in df :
+            dictionary_seasonnumber_episodenumber = []
             chunks.drop_duplicates()
             data_dict_seasonnumber_episodenumber = chunks.to_dict(orient = "records")
             for i in data_dict_seasonnumber_episodenumber :
@@ -14,12 +14,13 @@ class from_tsv_to_sql ():
                     pass
                 else :
                     dictionary_seasonnumber_episodenumber.append(i)
-        final_dataframe = pd.DataFrame.from_dict(dictionary_seasonnumber_episodenumber)
-        final_dataframe.to_csv("F:/taklif/cleaned_up/titleEpisode.csv",encoding='utf-8-sig' , index=False)
+            final_dataframe = pd.DataFrame.from_dict(dictionary_seasonnumber_episodenumber)
+            final_dataframe.to_csv("F:/taklif/cleaned_up/titleEpisode.csv",mode="a",header="False",encoding='utf-8-sig' , index=True)
     def titleAkas_cleanup() :
-        dictionary_dict_title_and_labguages = []
+        index_pk = 1
         df = pd.read_csv ("titleAkas.tsv", chunksize=50000 ,sep= "\t")
         for chunks in df :
+            dictionary_dict_title_and_labguages = []
             chunks.drop_duplicates()
             data_dict_title_and_labguages = chunks.to_dict(orient= "records")
             for i in data_dict_title_and_labguages :
@@ -28,13 +29,23 @@ class from_tsv_to_sql ():
                 elif i["language"] == "\\N" :
                     pass
                 else :
-                    dictionary_dict_title_and_labguages.append(i)
-        final_dataframe = pd.DataFrame.from_dict(dictionary_dict_title_and_labguages)
-        final_dataframe.to_csv("F:/taklif/cleaned_up/titleAkas.csv",encoding='utf-8-sig' , index=False)
-    def titleBasics_cleanup() :
-        dictionary_dict_isadult_runtime = []
+                    titleId =  i["titleId"]
+                    ordering = i["ordering"]
+                    title = i["title"]
+                    region = i["region"]
+                    language = i["language"]
+                    types = i["types"]
+                    attributes = i["attributes"]
+                    isOriginalTitle = i["isOriginalTitle"]
+                    remade_dictionary = { "index_pk" : f"{index_pk}" ,"titleId" : f"{titleId}" , "ordering" : f"{ordering}" , "title" : f"{title}" , "region" : f"{region}" , "language" : F"{language}" , "types" : f"{types}" , "attributes" : f"{attributes}" , "isOriginalTitle" : f"{isOriginalTitle}" }
+                    dictionary_dict_title_and_labguages.append(remade_dictionary)
+                    index_pk += 1 
+            final_dataframe = pd.DataFrame.from_dict(dictionary_dict_title_and_labguages)
+            final_dataframe.to_csv("F:/taklif/cleaned_up/titleAkas.csv",mode="a",header="False",encoding='utf-8-sig' , index=False)
+    def titleBasics_cleanup() :     
         df = pd.read_csv ("titleBasics.tsv", chunksize=50000 ,sep= "\t")
         for chunks in df :
+            dictionary_dict_isadult_runtime = []
             index_pk =1
             chunks.drop_duplicates()
             data_dict_isadult_runtime = chunks.to_dict(orient= "records")
@@ -56,19 +67,19 @@ class from_tsv_to_sql ():
                         remade_dictionary = { "index_pk" : f"{index_pk}" ,"tconst" : f"{tconst}" , "titleType" : f"{titleType}" , "primaryTitle" : f"{primaryTitle}" , "originalTitle" : f"{originalTitle}" , "isAdult" : F"{isAdult}" , "startYear" : f"{startYear}" , "endYear" : f"{endYear}" , "runtimeMinutes" : f"{runtimeMinutes}" , "genres" : f"{genres}"}
                         dictionary_dict_isadult_runtime.append(remade_dictionary)
                         index_pk += 1                     
-        final_dataframe = pd.DataFrame.from_dict(dictionary_dict_isadult_runtime)
-        final_dataframe.to_csv("F:/taklif/cleaned_up/titleBasics.csv",encoding='utf-8-sig' , index=False)
+            final_dataframe = pd.DataFrame.from_dict(dictionary_dict_isadult_runtime)
+            final_dataframe.to_csv("F:/taklif/cleaned_up/titleBasics.csv",mode="a",header="False",encoding='utf-8-sig' , index=False)
     def titleCrew_cleanup() :
-        dictionary_dict_directors_writers = []
+        index_pk = 1
         df = pd.read_csv ("titleCrew.tsv", chunksize=50000 ,sep= "\t")
         for chunks in df :
+            dictionary_dict_directors_writers = []
             chunks.drop_duplicates()
             data_dict_directors_writers = chunks.to_dict(orient= "records")
             for i in data_dict_directors_writers :
                 if i["directors"] == "\\N" :
                         pass
                 else :
-                    index_pk = 1
                     number_of_directors =  i["directors"].split(",")
                     number_of_writers = i["writers"].split(",")
                     for data_number_of_directors in number_of_directors   :
@@ -79,12 +90,12 @@ class from_tsv_to_sql ():
                                 remade_dictionary = { "index_pk" : f"{index_pk}" ,"tconst" : f"{tconst}" , "directors" : f"{directors}" , "writers" : f"{writers}"}
                                 dictionary_dict_directors_writers.append(remade_dictionary)
                                 index_pk += 1 
-        final_dataframe = pd.DataFrame.from_dict(dictionary_dict_directors_writers)
-        final_dataframe.to_csv("F:/taklif/cleaned_up/titleCrew.csv",encoding='utf-8-sig' , index=False)
+            final_dataframe = pd.DataFrame.from_dict(dictionary_dict_directors_writers)
+            final_dataframe.to_csv("F:/taklif/cleaned_up/titleCrew.csv",mode="a",header="False",encoding='utf-8-sig' , index=False)
     def titleRatings_cleanup() :
-        dictionary_dict_averagerating_numberofvotes = []
         df = pd.read_csv ("titleRatings.tsv", chunksize=50000 ,sep= "\t")
         for chunks in df :
+            dictionary_dict_averagerating_numberofvotes = []
             chunks.drop_duplicates()
             data_dict_averagerating_numberofvotes = chunks.to_dict(orient= "records")
             for i in data_dict_averagerating_numberofvotes :
@@ -94,13 +105,13 @@ class from_tsv_to_sql ():
                     pass
                 else :
                     dictionary_dict_averagerating_numberofvotes.append(i)
-        final_dataframe = pd.DataFrame.from_dict(dictionary_dict_averagerating_numberofvotes)
-        final_dataframe.to_csv("F:/taklif/cleaned_up/titleRatings.csv",encoding='utf-8-sig' , index=False)
+            final_dataframe = pd.DataFrame.from_dict(dictionary_dict_averagerating_numberofvotes)
+            final_dataframe.to_csv("F:/taklif/cleaned_up/titleRatings.csv",mode="a",header="False",encoding='utf-8-sig' , index=False)
     def nameBasics_cleanup() :
-        dictionary_dict_actornames = []
         df = pd.read_csv ("nameBasics.tsv", chunksize=50000 ,sep= "\t")
         index_pk = 1
         for chunks in df :
+            dictionary_dict_actornames = []
             chunks.drop_duplicates()
             data_dict_actornames = chunks.to_dict(orient= "records")
             for i in data_dict_actornames :
@@ -123,13 +134,13 @@ class from_tsv_to_sql ():
                             remade_dictionary = { "index_pk" : f"{index_pk}" ,"nconst" : f"{nconst}" , "primaryName" : f"{primaryName}" , "birthYear" : f"{birthYear}" , "deathYear" : f"{deathYear}" , "primaryProfession" : f"{primaryProfession}" , "knownForTitles" : f"{knownForTitles}"}
                             dictionary_dict_actornames.append(remade_dictionary)
                             index_pk += 1
-        final_dataframe = pd.DataFrame.from_dict(dictionary_dict_actornames)
-        final_dataframe.to_csv("F:/taklif/cleaned_up/nameBasics.csv",encoding='utf-8-sig',index=False)
+            final_dataframe = pd.DataFrame.from_dict(dictionary_dict_actornames)
+            final_dataframe.to_csv("F:/taklif/cleaned_up/nameBasics.csv",mode="a",header="False",encoding='utf-8-sig',index=False)
     def titlePrincipals_cleanup() :
         df = pd.read_csv ("titlePrincipals.tsv", chunksize=500000 ,low_memory=False,sep= "\t")
         for chunk in df :       
-            chunk.to_csv("F:/taklif/cleaned_up/titlePrincipals.csv",encoding='utf-8-sig' , index=False)
-from_tsv_to_sql.titleBasics_cleanup()
+            chunk.to_csv("F:/taklif/cleaned_up/titlePrincipals.csv",mode ="a",header="False",encoding='utf-8-sig' , index=True)
+from_tsv_to_sql.titleAkas_cleanup()
 """⠛⠋⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⣻⣩⣉⠉⠉
 ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⢀⣀⣀⣀⣀⣀⣀⡀⠄⠄⠉⠉⠄⠄⠄
 ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⣠⣶⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣤⠄⠄⠄⠄
@@ -163,5 +174,3 @@ while True :
         print (f"all done \n ----------------------------- ")
     else :
         print ("option not available ")
-
-
